190fb4eb15e070b99f6395d657e52740343ee1b7
Aakash Sondagar <89018309+Aakash-Sondagar@users.noreply.github.com>
2026-08-10 15:57:43 +0530

    Create Shipping.tsx

---
+++ apps/web/src/routes/store/Shipping.tsx
@@ -0,0 +1,11 @@
+import BlankPolicy from "./BlankPolicy";
+
+export default function Shipping() {
+  return (
+    <BlankPolicy
+      crumb="Shipping"
+      title="Shipping"
+      blurb="Delivery timelines, free-shipping rules, and courier partners will live here."
+    />
+  );
+}
