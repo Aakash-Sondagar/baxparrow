f1808ff780c5bf861fd88931d03c94963664b068
Aakash Sondagar <89018309+Aakash-Sondagar@users.noreply.github.com>
2026-08-10 15:56:44 +0530

    Create Returns.tsx

---
+++ apps/web/src/routes/store/Returns.tsx
@@ -0,0 +1,11 @@
+import BlankPolicy from "./BlankPolicy";
+
+export default function Returns() {
+  return (
+    <BlankPolicy
+      crumb="Returns"
+      title="Returns"
+      blurb="7-day retail return steps and conditions will be published here."
+    />
+  );
+}
