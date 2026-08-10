469e001bc9bdc88800f4efd511e0e9ce7c875693
Aakash Sondagar <89018309+Aakash-Sondagar@users.noreply.github.com>
2026-08-10 15:58:09 +0530

    Create Warranty.tsx

---
+++ apps/web/src/routes/store/Warranty.tsx
@@ -0,0 +1,11 @@
+import BlankPolicy from "./BlankPolicy";
+
+export default function Warranty() {
+  return (
+    <BlankPolicy
+      crumb="Warranty"
+      title="Warranty"
+      blurb="1-year manufacturing warranty terms will be spelled out here."
+    />
+  );
+}
